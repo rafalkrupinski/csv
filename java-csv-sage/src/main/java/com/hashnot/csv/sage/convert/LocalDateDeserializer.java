f54d22853feaf85a30161686ae0e193103dbfa2b
Rafał Krupiński <rkrupinski+github@gmail.com>
2016-02-07 14:39:12 +0000

    Fix LocalDateDeserializer empty string handling and error reporting

--- java-csv-sage/src/main/java/com/hashnot/csv/sage/convert/LocalDateDeserializer.java
+++ java-csv-sage/src/main/java/com/hashnot/csv/sage/convert/LocalDateDeserializer.java
@@ -1,25 +1,22 @@
 package com.hashnot.csv.sage.convert;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 
 /**
  * @author Rafał Krupiński
  */
 public class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
     @Override
-    public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        try {
-            return SageDate.formatter.parse(jsonParser.getText(), LocalDate::from);
-        } catch (DateTimeParseException e) {
-            throw new InvalidFormatException(jsonParser, "Error while parsing date", null, LocalDate.class);
-        }
+    public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        String text = jsonParser.getText();
+        if (text.isEmpty())
+            return null;
+
+        return SageDate.formatter.parse(text, LocalDate::from);
     }
 }
